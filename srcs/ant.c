fbe399bbf82e2d2ab79442cf35dcaecb255364ec
sregnard <stanley.regnard@gmail.com>
2019-06-12 13:51:40 +0200

    ant

--- srcs/ant.c
+++ srcs/ant.c
@@ -6,7 +6,7 @@
 /*   By: chrhuang <chrhuang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 16:11:11 by sregnard          #+#    #+#             */
-/*   Updated: 2019/06/12 13:43:25 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/06/12 13:49:42 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,9 @@ int		ants_init(t_room *r, int nb_ants)
 	return (SUCCESS);
 }
 
-/*/void	ant_free(t_ant **ptr)
+void	ant_free(t_ant **ptr)
 {
-		ft_printf("ant #%d freed\n", ((t_ant *)(*ptr))->id);
-		//ft_bzero(*ptr, size);
-		ft_memdel(ptr);
+		ft_memdel((void **)ptr);
 }
 
 void	ant_print(t_ant	*ant)
@@ -52,4 +50,3 @@ void	ant_print(t_ant	*ant)
 		ft_printf("#%-3d ", ant->id);
 		ant->moved ? ft_printf("moving\n") : ft_printf("waiting\n");
 }
-*/
