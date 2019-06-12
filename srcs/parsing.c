2d18418232cf9bd1c74c802ec18bd4c10585d015
chrhuang <christian.huang.49@gmail.com>
2019-06-12 13:41:29 +0200

    s

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,20 +6,20 @@
 /*   By: chrhuang <chrhuang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 18:22:20 by chrhuang          #+#    #+#             */
-/*   Updated: 2019/06/12 13:37:19 by chrhuang         ###   ########.fr       */
+/*   Updated: 2019/06/12 13:41:07 by chrhuang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 #include "libft.h"
 
-static int	get_ants(t_li *li, char *line, t_ant **ant_start)
+static int	get_ants(t_li *li, char *line, int *nb_ants)
 {
 	if (li == NULL || line == NULL)
 		trigger_error(li, "li or tab == NULL in get_ants function\n");
 	if (!ft_isinteger(line))
 		return (FAIL);
-	*ant_start = ants_init(ft_atoi(line));
+	*nb_ants = ft_atoi(line);
 	li->flags &= ~FLAG_ANT;
 	return (SUCCESS);
 }
@@ -27,7 +27,7 @@ static int	get_ants(t_li *li, char *line, t_ant **ant_start)
 int	parsing(t_li *li)
 {
 	char	*line;
-	t_ant	*start;
+	int		start;
 
 	line = NULL;
 	//li->flags |= FLAG_ANT;
