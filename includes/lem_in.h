b502a4f51ac5459c83b08dad1a14ccc7a31ce073
Stanley REGNARD <sregnard@e3r2p2.42.fr>
2019-06-25 15:44:40 +0200

    .h rework

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,12 +6,12 @@
 /*   By: chrhuang <chrhuang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 15:00:19 by sregnard          #+#    #+#             */
-/*   Updated: 2019/06/25 15:20:59 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/06/25 15:44:25 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#ifndef LEM_IN_H
+# define LEM_IN_H
 
 # include "libft.h"
 # include <stdbool.h>
@@ -29,98 +29,133 @@
 
 # define LI_BUFF_SIZE	4096
 
-typedef struct		s_room
-{
-	char			*name;
-	t_point			pos;
-	t_list_link		*links;
-	struct s_room	*parent;
-	struct s_room	*next;
-	struct s_room	*prev;
-	unsigned int	flags;
-}					t_room;
+/*
+**		ROOMS
+*/
 
-typedef struct		s_link
+typedef struct			s_room
 {
-	t_room			*dst;
-	unsigned int	flags;
-	struct s_link	*next;
-}					t_link;
+	char				*name;
+	t_point				pos;
+	struct s_list_link	*links;
+	struct s_room		*parent;
+	struct s_room		*next;
+	struct s_room		*prev;
+	unsigned int		flags;
+}						t_room;
 
-typedef struct		s_path
+typedef struct			s_list_room
 {
-	t_room			*room;
-	int				round;
-	struct s_path	*next;
-}					t_path;
+	t_room				*start;
+	t_room				*current;
+	t_room				*end;
+	int					size;
+}						t_list_room;
 
-typedef struct		s_ant
+enum					e_flags_room
 {
-	int				id;
-	bool			moved;
-	t_room			*room;
-	t_list_path	*path;
-	struct s_ant	*next;
-}					t_ant;
+	FLAG_VISITED = (1 << 0),
+	FLAG_RESERVED = (2 << 0),
+	FLAG_QUEUED = (3 << 0),
+};
 
-typedef struct		s_queue
+/*
+**		LINKS
+*/
+
+typedef struct			s_link
 {
-	t_room			*room;
-	struct s_queue	*next;
-}					t_queue;
+	t_room				*dst;
+	unsigned int		flags;
+	struct s_link		*next;
+}						t_link;
 
-typedef struct		s_list_room
+typedef struct			s_list_link
 {
-	t_room			*start;
-	t_room			*current;
-	t_room			*end;
-	int				size;
-}					t_list_room;
+	t_link				*first;
+	t_link				*current;
+	t_link				*last;
+	int					size;
+}						t_list_link;
 
-typedef struct		s_list_link
+enum					e_flags_link
 {
-	t_link			*first;
-	t_link			*current;
-	t_link			*last;
-	int				size;
-}
+	FLAG_USED = (1 << 0),
+};
 
-typedef struct		s_list_path
+/*
+**		PATHS
+*/
+
+typedef struct			s_path
 {
-	t_path			*start;
-	t_path			*current;
-	t_path			*end;
-	int				size;
-}
+	t_room				*room;
+	int					round;
+	struct s_path		*next;
+}						t_path;
 
-typedef struct		s_list_ant
+typedef struct			s_list_path
 {
-	t_ant			*first;
-	t_ant			*current;
-	t_ant			*last;
-	int				size;
-}
+	t_path				*start;
+	t_path				*current;
+	t_path				*end;
+	int					size;
+}						t_list_path;
 
-typedef struct		s_list_queue
+/*
+**		ANTS
+*/
+
+typedef struct			s_ant
 {
-	t_queue			*first;
-	t_queue			*current
-	t_queue			*last;
-}					t_list_queue;
+	int					id;
+	bool				moved;
+	t_room				*room;
+	t_list_path			*path;
+	struct s_ant		*next;
+}						t_ant;
 
-typedef struct		s_li
+typedef struct			s_list_ant
 {
-	char			buf[LI_BUFF_SIZE];
-	unsigned int	index;
-	t_list_rooms	*rooms;
-	t_list_ants		*ants;
-	t_list_queue	*queue;
-	int				nb_ants;
-	int				moves;
-	unsigned int	flags;
-}					t_li;
+	t_ant				*first;
+	t_ant				*current;
+	t_ant				*last;
+	int					size;
+}						t_list_ant;
 
-enum				e_flags
+/*
+**		QUEUE
+*/
+
+typedef struct			s_queue
+{
+	t_room				*room;
+	struct s_queue		*next;
+}						t_queue;
+
+typedef struct			s_list_queue
+{
+	t_queue				*first;
+	t_queue				*current;
+	t_queue				*last;
+}						t_list_queue;
+
+/*
+**		LEM-IN
+*/
+
+typedef struct			s_li
+{
+	char				buf[LI_BUFF_SIZE];
+	unsigned int		index;
+	t_list_room			*rooms;
+	t_list_ant			*ants;
+	t_list_queue		*queue;
+	int					moves;
+	unsigned int		flags;
+}						t_li;
+
+enum					e_flags
 {
 	FLAG_START = (1 << 0),
 	FLAG_END = (1 << 1),
@@ -129,87 +164,76 @@ enum				e_flags
 	FLAG_LINK = (1 << 4),
 };
 
-enum				e_flags_room
-{
-	FLAG_VISITED = (1 << 0),
-	FLAG_RESERVED = (2 << 0),
-	FLAG_QUEUED = (3 << 0),
-};
-
-enum				e_flags_link
-{
-	FLAG_USED = (1 << 0),
-};
-
 /*
-**
+**						lem_in.c
 */
 
-int					free_all(t_li *li);
-int					li_buffer(t_li *li, const char *s, size_t len);
+int						free_all(t_li *li);
+int						li_buffer(t_li *li, const char *s, size_t len);
 
 /*
-**		Parsing
+**						parsing.c
+**						parsing_links.c
+**						parsing_rooms.c
 */
 
-int					parsing(t_li *li);
-int					get_room(t_li *li, char *line, t_room **last);
-int					get_link(t_li *li, char *line);
-int					is_link(t_li *li, char **line);
-
+int						parsing(t_li *li);
+int						get_room(t_li *li, char *line, t_room **last);
+int						get_link(t_li *li, char *line);
+int						is_link(t_li *li, char **line);
 
 /*
-**		error.c
+**						error.c
 */
 
-void				trigger_error(t_li *li, char *error);
+void					trigger_error(t_li *li, char *error);
 
 /*
-**		room.c
+**						room.c
 */
 
-t_room				*room_new(char *name, t_point *pos);
-void				room_add(t_room **rooms, t_room *new_room);
-void				room_free(t_room **ptr);
-void				room_print(t_room *room);
-void				room_clean(t_li *li, t_room *room);
+t_room					*room_new(char *name, t_point *pos);
+void					room_add(t_room **rooms, t_room *new_room);
+void					room_free(t_room **ptr);
+void					room_print(t_room *room);
+void					room_clean(t_li *li, t_room *room);
 
 /*
-**		links.c
+**						links.c
 */
 
-int					link_new(t_li *li, t_room *a, t_room *b);
-void				link_free(t_link **ptr);
-void				link_print(t_link *link);
-void				link_clean(t_li *li, t_link *link);
+int						link_new(t_li *li, t_room *a, t_room *b);
+void					link_free(t_link **ptr);
+void					link_print(t_link *link);
+void					link_clean(t_li *li, t_link *link);
 
 /*
-**		path.c
+**						path.c
 */
 
-int					path_init(t_li *li);
-int					path_insert(t_ant *ant, t_room *room, int round);
-void				path_print(t_path *path);
+int						path_init(t_li *li);
+int						path_insert(t_ant *ant, t_room *room, int round);
+void					path_print(t_path *path);
 
 /*
-**		ant.c
+**						ant.c
 */
 
-int 				ants_init(t_li *li, int nb_ants);
-void				ant_free(t_ant **ptr);
-void				ant_print(t_ant *ant);
-void				ant_print_all(t_li *li);
+int						ants_init(t_li *li, int nb_ants);
+void					ant_free(t_ant **ptr);
+void					ant_print(t_ant *ant);
+void					ant_print_all(t_li *li);
 
 /*
-**		move_ant.c
+**						move_ant.c
 */
 
-int					move_ant(t_ant *ant, t_room *dst);
+int						move_ant(t_ant *ant, t_room *dst);
 
 /*
-**		bfs.c
+**						bfs.c
 */
 
-int					bfs(t_li *li, int turn);
+int						bfs(t_li *li, int turn);
 
 #endif
