493e52341cc523d7fd0cc6bd2f7d27abdecfbade
Stanley REGNARD <sregnard@e2r2p14.42.fr>
2019-07-17 14:09:29 +0200

    changed buff size from 1 to 10000

--- libft/includes/get_next_line.h
+++ libft/includes/get_next_line.h
@@ -6,16 +6,14 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/11 15:08:48 by sregnard          #+#    #+#             */
-/*   Updated: 2019/06/27 15:15:16 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/07/17 14:08:04 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 10000
 # define FD_MAX 4864
-# define EOL '\n'
-# define EOS '\0'
 
 # include <unistd.h>
 # include <stdlib.h>
