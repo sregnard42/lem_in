a9f12e8f3eb691e45eaa212fddce6af8ba1a466f
sregnard <stanley.regnard@gmail.com>
2019-08-11 14:25:51 +0200

    opti ants

--- srcs/ant.c
+++ srcs/ant.c
@@ -6,27 +6,24 @@
 /*   By: chrhuang <chrhuang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 16:11:11 by sregnard          #+#    #+#             */
-/*   Updated: 2019/08/10 17:18:11 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/08/11 14:24:06 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+/*
+**		malloc of nb_ants + 1 because ant[0] isn't used
+*/
 
 int			ants_init(t_li *li)
 {
 	int	i;
 
 	i = 0;
-	if (!(li->ants = (t_ant **)malloc(sizeof(t_ant *) * (li->nb_ants + 2))))
-		trigger_error(li, "Init ants : Malloc FAIL\n");
-	ft_bzero(li->ants, sizeof(t_ant *) * (li->nb_ants + 2));
-	while (++i < li->nb_ants + 1)
-	{
-		if (!(li->ants[i] = (t_ant *)malloc(sizeof(t_ant))))
-			trigger_error(li, "Init ants : Malloc FAIL\n");
-		ft_bzero(li->ants[i], sizeof(t_ant));
-		li->ants[i]->id = i;
-	}
+	if (!(li->ants = (t_ant *)malloc(sizeof(t_ant) * (li->nb_ants + 1))))
+		trigger_error(li, "Init ants : Error malloc\n");
+	ft_bzero(li->ants, sizeof(t_ant) * (li->nb_ants + 1));
 	return (SUCCESS);
 }
 
@@ -36,83 +33,82 @@ void	ants_set_stage(t_li *li)
 	int			j;
 	t_list_path	*paths;
 
-	paths = li->paths_opti[0];
-	paths->current = paths->first;
 	i = 1;
 	j = 1;
+	paths = li->paths_opti[0];
+	paths->current = paths->first;
 	while (paths->current)
 	{
 		j = i;
 		paths->current->ant = i;
-		li->ants[i]->flags |= FLAG_LEAD;
-		li->ants[i]->flags |= FLAG_MOVING;
+		li->ants[i].flags |= FLAG_LEAD;
+		li->ants[i].flags |= FLAG_MOVING;
 		while (i < paths->current->capacity + j)
-			li->ants[i++]->stage = paths->current->start;
+		{
+			li->ants[i].id = i;
+			li->ants[i++].stage = paths->current->start;
+		}
 		paths->current = paths->current->next;
 	}
 }
 
-int	ant_move(t_li *li, t_ant **ant)
+int	ant_move(t_li *li, t_ant *ant)
 {
-	if (!((*ant)->flags & FLAG_DEPARTED) &&
-	(*ant)->stage->room != li->rooms->end)
+	if (!(ant->flags & FLAG_DEPARTED) && ant->stage->room != li->rooms->end)
 	{
-		(*ant)->flags |= FLAG_DEPARTED;
+		ant->flags |= FLAG_DEPARTED;
 		ft_printf("\033[1;32m");
 	}
 	else
 	{
-		(*ant)->stage->room == li->rooms->end ? ft_printf("\033[1;31m"):
+		ant->stage->room == li->rooms->end ? ft_printf("\033[1;31m"):
 		ft_printf("\033[1;33m");
 	}
-	ft_printf("L%d-%s", (*ant)->id, (*ant)->stage->room->name);
+	ft_printf("L%d-%s", ant->id, ant->stage->room->name);
 		ft_printf("\033[0m");
-	if ((*ant)->stage->room == li->rooms->end)
-		(*ant)->flags |= FLAG_ARRIVED;
-	(*ant)->stage = (*ant)->stage->next;
-	return ((*ant)->flags & FLAG_ARRIVED ? SUCCESS : FAIL);
+	if (ant->stage->room == li->rooms->end)
+		ant->flags |= FLAG_ARRIVED;
+	ant->stage = ant->stage->next;
+	return (ant->flags & FLAG_ARRIVED ? 1 : 0);
 }
 
-void	ants_move_path(t_li *li, int *i, t_list_path *paths, int *nb_arrived)
+static int	ants_move_path(t_li *li, t_list_path *paths)
 {
-	*i = 0;
-	while (*i + paths->current->ant <= li->nb_ants &&
-	li->ants[paths->current->ant + *i]->flags & FLAG_MOVING)
-    {
-    	if (*i > 0 && li->ants[paths->current->ant + *i]->flags & FLAG_LEAD)
-			break ;
-		if (!(li->ants[paths->current->ant + *i]->flags & FLAG_ARRIVED))
+	int	i;
+	int	id;
+	int	nb_arrived;
+
+	i = 0;
+	id = paths->current->ant;
+	nb_arrived = 0;
+	while (id + i <= li->nb_ants && li->ants[id + i].flags & FLAG_MOVING
+		&& !(i > 0 && li->ants[id + i].flags & FLAG_LEAD))
+	 {
+		if (!(li->ants[id + i].flags & FLAG_ARRIVED))
 		{
 			li->flags & FLAG_SP ? ft_printf(" ") : (li->flags |= FLAG_SP);
-			if (ant_move(li, &li->ants[paths->current->ant + *i]) == SUCCESS)
-			{
-				++(*nb_arrived);
-	    		if (li->ants[paths->current->ant + 1])
-					if (li->ants[paths->current->ant + 1]->flags & FLAG_LEAD)
-		  				++paths->current->ant;
-	    		}
-			}
-		++(*i);
+			nb_arrived += ant_move(li, &li->ants[id + i]);
+		}
+		++i;
 	}
+	id + i <= li->nb_ants ? li->ants[id + i].flags |= FLAG_MOVING : 0;
+	return (nb_arrived);
 }
 
-void	ants_move(t_li *li)
+void		ants_move(t_li *li)
 {
 	t_list_path	*paths;
-	int			nb_ant_arrived;
-	int			i;
+	int			nb_arrived;
 
 	paths = li->paths_opti[0];
-	nb_ant_arrived = 0;
-	while (nb_ant_arrived < li->nb_ants)
+	nb_arrived = 0;
+	while (nb_arrived < li->nb_ants)
 	{
 		li->flags &= ~FLAG_SP;
 		paths->current = paths->first;
 		while (paths->current)
 		{
-			ants_move_path(li, &i, paths, &nb_ant_arrived);
-			if (li->ants[paths->current->ant + i])
-				li->ants[paths->current->ant + i]->flags |= FLAG_MOVING;
+			nb_arrived += ants_move_path(li, paths);
 			paths->current = paths->current->next;
 		}
 		ft_printf("\n");
