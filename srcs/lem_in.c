1857dfe5a01c89a8756a59e9513d7fa62767b916
sregnard <stanley.regnard@gmail.com>
2019-08-15 17:06:16 +0200

    comment pathfindingg

--- srcs/lem_in.c
+++ srcs/lem_in.c
@@ -6,7 +6,7 @@
 /*   By: chrhuang <chrhuang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 14:53:46 by sregnard          #+#    #+#             */
-/*   Updated: 2019/08/15 16:05:54 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/08/15 17:01:45 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,18 +115,20 @@ int			main(void)
 	ft_printf("---Moving ants---------------------------------------------\n");
 	ft_printf("\033[0m");
 	ants_set_stage(&li);
-	ants_move(&li);
+	//ants_move(&li);
 	ft_printf("\033[1m");
 	ft_printf("---Moving ants END-----------------------------------------\n");
 	ft_printf("Set of paths used :\n");
 	path_print_all(li.paths_opti[0]);
 	ft_printf("\033[0m");
+	/*
 	ft_printf("\033[1;36m");
 	ft_printf("Pathfinding :\n");
 	pathfinding(&li);
 	ft_printf("All possible paths :\n");
 	path_print_all(li.pathfinding);
 	ft_printf("\033[0m");
+	*/
 //	room_print_all(&li);
 //	free_all(&li);
 	return (SUCCESS);
