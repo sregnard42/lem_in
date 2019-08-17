d6a6ed629fea107dff862aa5127426bb524b45c8
sregnard <stanley.regnard@gmail.com>
2019-08-17 23:31:05 +0200

    included all generator options

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
+#maps/generator --flow-one > tmp.map
+#maps/generator --flow-ten > tmp.map
+#maps/generator --flow-thousand > tmp.map
+#maps/generator --big > tmp.map
 maps/generator --big-superposition > tmp.map
 ./lem-in < tmp.map
 tail -1 tmp.map
