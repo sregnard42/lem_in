6587d91a070b01cb91e92eb36abf21c227667dbe
sregnard <stanley.regnard@gmail.com>
2019-09-02 17:48:22 +0200

    ptcmp

--- libft/includes/point.h
+++ libft/includes/point.h
@@ -6,7 +6,7 @@
 /*   By: chrhuang <chrhuang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 14:18:30 by sregnard          #+#    #+#             */
-/*   Updated: 2019/06/11 19:22:08 by chrhuang         ###   ########.fr       */
+/*   Updated: 2019/09/02 17:15:12 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ typedef struct	s_point
 t_point			*ft_ptnew(int x, int y, int z);
 void			ft_ptcpy(t_point *dst, t_point *src);
 void			ft_ptset(t_point *pt, int x, int y, int z);
+int				ft_ptcmp(t_point *pt1, t_point *pt2);
 void			ft_ptprint(t_point *pt);
 void			ft_ptfree(void **pt, size_t size);
 
