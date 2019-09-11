b7fe583feeb68bacbaae995ed118170b5c85d3c2
chrhuang <christian.huang.49@gmail.com>
2019-09-11 11:20:18 +0200

    change ant + 1 to ant because overflow in int max

--- srcs/ant/ant.c
+++ srcs/ant/ant.c
@@ -6,14 +6,14 @@
 /*   By: chrhuang <chrhuang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 16:11:11 by sregnard          #+#    #+#             */
-/*   Updated: 2019/09/08 13:50:25 by chrhuang         ###   ########.fr       */
+/*   Updated: 2019/09/11 11:17:04 by chrhuang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
 /*
-**		malloc of nb_ants + 1 because ant[0] isn't used
+**		malloc of nb_ants
 */
 
 int			ants_init(t_li *li)
@@ -21,7 +21,7 @@ int			ants_init(t_li *li)
 	int	i;
 
 	i = 0;
-	if (!(li->ants = (t_ant *)ft_memalloc(sizeof(t_ant) * (li->nb_ants + 1))))
+	if (!(li->ants = (t_ant *)ft_memalloc(sizeof(t_ant) * (li->nb_ants))))
 		trigger_error(li, "Init ants : Error malloc\n");
 	return (SUCCESS);
 }
@@ -32,8 +32,7 @@ void		ants_set_stage(t_li *li)
 	int			j;
 	t_list_path	*paths;
 
-	i = 1;
-	j = 1;
+	i = 0;
 	paths = li->paths_opti[0];
 	paths->current = paths->first;
 	while (paths->current)
@@ -63,7 +62,7 @@ static int	ant_move(t_li *li, t_ant *ant)
 		ant->stage->room == li->rooms->end ? ft_printf("%s", RED) :
 			ft_printf("%s", YELLOW);
 	}
-	ft_printf("L%d-%s", ant->id, ant->stage->room->name);
+	ft_printf("L%d-%s", ant->id + 1, ant->stage->room->name);
 	li->flags & FLAG_COLOR ? ft_printf("%s", RESET) : 0;
 	if (ant->stage->room == li->rooms->end)
 		ant->flags |= FLAG_ARRIVED;
@@ -83,7 +82,7 @@ static int	ants_move_path(t_li *li, t_path *path)
 
 	id = path->ant;
 	arrived = 0;
-	while (id <= li->nb_ants && li->ants[id].flags & FLAG_MOVING
+	while (id < li->nb_ants && li->ants[id].flags & FLAG_MOVING
 		&& !(id - path->ant > 0 && li->ants[id].flags & FLAG_LEAD))
 	{
 		if (!(li->ants[id].flags & FLAG_ARRIVED))
@@ -93,14 +92,14 @@ static int	ants_move_path(t_li *li, t_path *path)
 			if (ant_move(li, &li->ants[id]))
 			{
 				++arrived;
-				if (path->ant < li->nb_ants &&
+				if (path->ant < li->nb_ants - 1 &&
 					!(li->ants[path->ant + 1].flags & FLAG_LEAD))
 					++path->ant;
 			}
 		}
 		++id;
 	}
-	id <= li->nb_ants && !(li->ants[id].flags & FLAG_LEAD) ?
+	id < li->nb_ants && !(li->ants[id].flags & FLAG_LEAD) ?
 	li->ants[id].flags |= FLAG_MOVING : 0;
 	return (arrived);
 }
